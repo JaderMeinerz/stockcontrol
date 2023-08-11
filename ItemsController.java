a02e1af3141728c548c6f5f87d2386b3fa49dd47
JaderMeinerz <jadermeinerzdossantos@yahoo.com.br>
2023-08-11 12:32:52 -0300

    Function update, findById and findAll created

--- ItemsController.java
+++ ItemsController.java
@@ -1,13 +1,56 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class ItemsController {
+    List<Items> items =new ArrayList<Items>();
 
-    public Items setItems(int id, String nome, int quantidade){
-        Items items = new Items();
-        items.setId(id);
-        items.setNome(nome);
-        items.setQuantidade(quantidade);
-
-        System.out.println(items);
-        return items;
+    public void setItems(int id, String nome, int quantidade){
+        items.add(new Items(id, nome, quantidade));
+        System.out.println("Cadastro concluido!"); 
     }
 
+    public void findAll(){
+        System.out.println("ID | Nome | Quantidade");
+        System.out.println("------------------------");
+        for (int i = 0; i<items.size();i++) {
+            Items item = items.get(i);
+            int id = item.getId();
+            String nome = item.getNome();
+            int quantidade = item.getQuantidade();
+            System.out.println(id + " | " + nome + " | " + quantidade);
+        }
+        
+    }
+    public Items findById(int id){
+        Items item = items.get(id-1);
+        return item;
+    }
+
+
+    public void update(int id){
+        Items item = items.get(id-1);
+        Scanner num = new Scanner(System.in);
+        Scanner str = new Scanner(System.in);
+        System.out.println("Qual campo deseja atualizar? ");
+        System.out.println("[1] Nome;");
+        System.out.println("[2] Quantidade; ");
+        int res = num.nextInt();
+        switch (res) {
+            case 1:
+                System.out.println("Qual o novo Nome do item? ");
+                String nome = str.nextLine();
+                item.setNome(nome);
+                break;
+            case 2:
+                System.out.println("Qual a nova Quantidade do item? ");
+                String quantidade = num.nextLine();
+                item.setNome(quantidade);
+                break;
+            default:
+                break;
+        }
+        System.out.println("Item atualizado com sucesso!");
+
+    }
 }
--- StockControl.java
+++ StockControl.java
@@ -4,26 +4,81 @@ import java.util.Scanner;
  * StockControl
  */
 public class StockControl {
+    static ItemsController itemsController = new ItemsController();
 
+    public static void save(){
+        
+        System.out.println("Qual o id do item a ser adicionado? ");
+        Scanner num = new Scanner(System.in);
+        Scanner str = new Scanner(System. in);
+        int id = num.nextInt();
+    
+        System.out.println("Qual o nome do item a ser adicionado? ");
+        String nome = str.nextLine();
+
+        System.out.println("Qual a quantidade do item a ser adicionado? ");
+        int quantidade = num.nextInt();
+
+        System.out.println("Id: " + id + " Nome: " + nome + " Quantidade: " + quantidade);
+    
+        itemsController.setItems(id, nome, quantidade);
+
+        menu();
+    };
+    public static void update(){
+        System.out.println("Qual o ID do item que deseja atualizar? ");
+        Scanner num = new Scanner(System.in);
+        int id = num.nextInt();
+
+        Items item = itemsController.findById(id);
+        String nome = item.getNome();
+        int quantidade = item.getQuantidade();
+
+        System.out.println("Id: " + id + " Nome: " + nome + " Quantidade: " + quantidade);
+
+        itemsController.update(id);
+
+        menu();
+    }
+
+    public static void menu(){
+        System.out.println("Deseja realizar alguma operação?[y/n]" );
+        Scanner t = new Scanner(System.in);
+        String res = t.nextLine();
+        if (res.equals("y")){
+            boolean tf = true;
+            if (tf) {
+            System.out.println("[1] Cadastro de itens;");
+            System.out.println("[2] Consulta de itens;");
+            System.out.println("[3] Atualizar um item;");
+            System.out.println("Qual função deseja efetuar? ");
+            Scanner s = new Scanner(System.in);
+            int op = s.nextInt();
+            
+            switch (op) {
+                case 1:
+                    save();
+                    break;
+    
+                case 2:
+                    itemsController.findAll();
+                    break;
+    
+                case 3:
+                    update();
+                    break;
+                default:
+        
+                    break;
+            }
+        }
+        } else { 
+            System.out.println("Até logo!");
+        }
+        
+    }
     public static void main(String[] args) {
-    
-    System.out.println("Qual o id do item a ser adicionado? ");
-    Scanner num = new Scanner(System.in);
-    Scanner str = new Scanner(System. in);
-    int id = num.nextInt();
-    
-    System.out.println("Qual o nome do item a ser adicionado? ");
-    String nome = str.nextLine();
-
-    System.out.println("Qual a quantidade do item a ser adicionado? ");
-    int quantidade = num.nextInt();
-
-    System.out.println("Id: " + id + "Nome: " + nome + "Quantidade: " + quantidade);
-    
-    ItemsController itemsController = new ItemsController();
-
-    itemsController.setItems(id, nome, quantidade);
-   
+        menu();
     
     }
 }
